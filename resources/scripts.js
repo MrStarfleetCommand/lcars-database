e732b106b53728c04dc3c2acf3f71d66158533cf
Linus Tucker <172833324+MrStarfleetCommand@users.noreply.github.com>
2026-04-09 19:56:59 -0400

    Interface generation work

--- resources/scripts.js
+++ resources/scripts.js
@@ -1,22 +1,7 @@
 import {txtToHtml, htmlToTxt, fetchSourceText} from '/parser.js';
 (async () => {
-	const sourceText = await fetchSourceText();
-	const contentArea = document.createElement('div');
-	const pageHeading = document.createElement('h2');
-	const parserOutput = document.createElement('div');
 	const waveformWrapper = document.createElement('div');
 	const waveform = document.createElement('div');
-	const numberOfPanels = Math.round((screen.height - 5) / 130);
-	const blues = ['light-blue', 'dark-blue'];
-	const grays = ['light-gray', 'dark-gray'];
-	const digits = Math.max(2, String(numberOfPanels).length);
-
-	pageHeading.innerText = document.title;
-	parserOutput.id = 'parser-output';
-	parserOutput.innerHTML = txtToHtml(sourceText);
-	contentArea.classList.add('content-area', 'scrollbox');
-	contentArea.append(pageHeading);
-	contentArea.append(parserOutput);
 	waveformWrapper.classList.add('waveform-wrapper');
 	waveform.classList.add('waveform');
 
@@ -29,13 +14,15 @@ import {txtToHtml, htmlToTxt, fetchSourceText} from '/parser.js';
 	waveformWrapper.append(waveform);
 	document.body.append(waveformWrapper);
 
+	const numberOfPanels = Math.round((screen.height - 5) / 130);
+	const digits = Math.max(2, String(numberOfPanels).length);
+	const blues = ['light-blue', 'dark-blue'];
+	const grays = ['light-gray', 'dark-gray'];
+
 	for (let i = 0; i < numberOfPanels; i++){
 		const panelOne = document.createElement('div');
 		const panelTwo = document.createElement('div');
-		const panelOneText = document.createElement('span');
-		const panelTwoText = document.createElement('span');
 		const prefix = String(i + 1).padStart(digits, 0);
-
 		let paletteOne;
 		let paletteTwo;
 
@@ -65,16 +52,11 @@ import {txtToHtml, htmlToTxt, fetchSourceText} from '/parser.js';
 		const colorOne = paletteOne[r(paletteOne.length)];
 		const colorTwo = paletteTwo[r(paletteTwo.length)];
 
-		panelOne.classList.add(colorOne, 'panel', 'panel-left');
-		panelTwo.classList.add(colorTwo, 'panel', 'panel-right');
-		panelOneText.classList.add('margin');
-		panelTwoText.classList.add('margin');
-		panelOneText.innerText = i === 0 ? 'LCARS 40274' : prefix + '-' + r(10) + r(10) + r(10) + r(10) + r(10) + r(10);
-		panelTwoText.innerText = i === 0 ? 'LCARS 40274' : prefix + '-' + String(r(1000000)).padStart(6, 0);
-		panelOne.append(panelOneText);
-		panelTwo.append(panelTwoText);
-		document.body.append(panelOne);
-		document.body.append(panelTwo);
+		panelOne.classList.add(colorOne, 'panel', 'box-with-label', 'panel-left');
+		panelTwo.classList.add(colorTwo, 'panel', 'box-with-label', 'panel-right');
+		panelOne.innerText = i === 0 ? 'LCARS 40274' : prefix + '-' + r(10) + r(10) + r(10) + r(10) + r(10) + r(10);
+		panelTwo.innerText = i === 0 ? 'LCARS 40274' : prefix + '-' + String(r(1000000)).padStart(6, 0);
+		document.body.append(panelOne, panelTwo);
 	}
 
 	hideExcessPanels();
@@ -113,24 +95,11 @@ import {txtToHtml, htmlToTxt, fetchSourceText} from '/parser.js';
 	const header2 = document.createElement('a');
 	const header3 = document.createElement('a');
 	const header4 = document.createElement('a');
-	const header1Text = document.createElement('span');
-	const header2Text = document.createElement('span');
-	const header3Text = document.createElement('span');
-	const header4Text = document.createElement('span');
 
-	header1Text.classList.add('margin');
-	header2Text.classList.add('margin');
-	header3Text.classList.add('margin');
-	header4Text.classList.add('margin');
-	header1Text.append('Home');
-	header2Text.append('Timeline');
-	header3Text.append('Trek Analyzed');
-	header4Text.append('Trek Lore');
-
-	header1.classList.add('button', 'header-one', 'dark-blue');
-	header2.classList.add('button', 'header-two', 'light-blue');
-	header3.classList.add('button', 'header-three', 'red');
-	header4.classList.add('button', 'header-four', 'light-gray');
+	header1.classList.add('button', 'box-with-label', 'header-one', 'dark-blue');
+	header2.classList.add('button', 'box-with-label', 'header-two', 'light-blue');
+	header3.classList.add('button', 'box-with-label', 'header-three', 'red');
+	header4.classList.add('button', 'box-with-label', 'header-four', 'light-gray');
 	header1.href = '/lcars-database/';
 	header2.href = '/lcars-database/timeline/';
 	header3.href = '/lcars-database/trek-analyzed/';
@@ -139,33 +108,35 @@ import {txtToHtml, htmlToTxt, fetchSourceText} from '/parser.js';
 	header2.addEventListener('click', beep0);
 	header3.addEventListener('click', beep0);
 	header4.addEventListener('click', beep0);
-	header1.append(header1Text);
-	header2.append(header2Text);
-	header3.append(header3Text);
-	header4.append(header4Text);
+	header1.append('Home');
+	header2.append('Timeline');
+	header3.append('Trek Analyzed');
+	header4.append('Trek Lore');
 
-	document.body.append(header1);
-	document.body.append(header2);
-	document.body.append(header3);
-	document.body.append(header4);
+	document.body.append(header1, header2, header3, header4);
 
+	const barOne = document.createElement('div');
+	const barTwo = document.createElement('div');
+	const barBreak = document.createElement('div');
+	const barContinuedOne = document.createElement('div');
+	const barContinuedTwo = document.createElement('div');
+	const marker = document.createElement('div');
 
+	barOne.classList.add('bar', 'number-one', 'dark-blue');
+	barTwo.classList.add('bar', 'number-two', 'dark-blue');
+	barBreak.classList.add('bar-break', 'light-gray');
+	barContinuedOne.classList.add('bar-continued', 'number-one', 'light-gray');
+	barContinuedTwo.classList.add('bar-continued', 'number-two', 'light-blue');
+	marker.classList.add('marker');
 
-	document.body.append(contentArea);
-
+	document.body.append(barOne, barTwo, barBreak, barContinuedOne, barContinuedTwo, marker);
 	document.body.innerHTML += `
-<div class="bar number-one dark-blue"></div>
-<div class="bar number-two dark-blue"></div>
-<div class="bar-break light-gray"></div>
-<div class="bar-continued number-one light-gray"></div>
-<div class="bar-continued number-two light-blue"></div>
-<div class="marker"></div>
-<div class="button sidebar-1 left-facing red" onclick="beep0()"><span class="margin">03-975683</span></div>
-<div class="button sidebar-2 left-facing light-gray" onclick="beep0()"><span class="margin">04-765466</span></div>
-<div class="button sidebar-3 left-facing dark-blue" onclick="beep0()"><span class="margin">05-224353</span></div>
-<div class="button sidebar-4 square border-button" onclick="beep2()"><span class="margin">03-975683</span></div>
-<div class="button sidebar-5 square border-button" onclick="beep2()"><span class="margin">04-765466</span></div>
-<div class="button sidebar-6 square border-button-cyan" onclick="beep1()"><span class="margin">05-224353</span></div>
+<div class="button box-with-label sidebar-1 left-facing red" onclick="beep0()">03-975683</div>
+<div class="button box-with-label sidebar-2 left-facing light-gray" onclick="beep0()">04-765466</div>
+<div class="button box-with-label sidebar-3 left-facing dark-blue" onclick="beep0()">05-224353</div>
+<div class="button box-with-label sidebar-4 square border-button" onclick="beep2()">03-975683</div>
+<div class="button box-with-label sidebar-5 square border-button" onclick="beep2()">04-765466</div>
+<div class="button box-with-label sidebar-6 square border-button-cyan" onclick="beep1()">05-224353</div>
 <div class="version-info">
 	<ul>
 		<li>Operating Version: v104.04.720</li>
@@ -175,33 +146,44 @@ import {txtToHtml, htmlToTxt, fetchSourceText} from '/parser.js';
 		<li>Current Operational Load: 6 Kiloquads</li>
 	</ul>
 </div>
-<div class="button sidebar-7 left-facing red blink-slow" onclick="beep0()"><span class="margin">03-975683</span></div>
-<div class="button sidebar-8 left-facing light-blue blink-fast" onclick="beep0()"><span class="margin">04-765466</span></div>
-<div class="button sidebar-9 left-facing dark-gray" onclick="beep0()"><span class="margin">05-224353</span></div>
-<div class="button sidebar-10 right-facing light-gray" onclick="beep0()"><span class="margin">03-975683</span></div>
-<div class="button sidebar-11 right-facing red" onclick="beep0()"><span class="margin">04-765466</span></div>
-<div class="button sidebar-12 right-facing dark-blue" onclick="beep0()"><span class="margin">05-224353</span></div>
-<a href="/lcars-database/legalities.html" class="left-facing copyrights button red" onclick="beep1()"><span class="margin">Legalities</span></a>`;
+<div class="button box-with-label sidebar-7 left-facing red blink-slow" onclick="beep0()">03-975683</div>
+<div class="button box-with-label sidebar-8 left-facing light-blue blink-fast" onclick="beep0()">04-765466</div>
+<div class="button box-with-label sidebar-9 left-facing dark-gray" onclick="beep0()">05-224353</div>
+<div class="button box-with-label sidebar-10 right-facing light-gray" onclick="beep0()">03-975683</div>
+<div class="button box-with-label sidebar-11 right-facing red" onclick="beep0()">04-765466</div>
+<div class="button box-with-label sidebar-12 right-facing dark-blue" onclick="beep0()">05-224353</div>
+<a href="/lcars-database/legalities.html" class="left-facing copyrights button box-with-label red" onclick="beep1()">Legalities</a>`;
 
-	const beepZero = new Audio('/lcars-database/resources/beep-0.mp3');
-	const beepOne = new Audio('/lcars-database/resources/beep-1.mp3');
-	const beepTwo = new Audio('/lcars-database/resources/beep-2.mp3');
+	const contentArea = document.createElement('div');
+	const pageHeading = document.createElement('h2');
+	const parserOutput = document.createElement('div');
+	const sourceText = await fetchSourceText();
+
+	pageHeading.innerText = document.title;
+	parserOutput.id = 'parser-output';
+	parserOutput.append(txtToHtml(sourceText));
+	contentArea.classList.add('content-area', 'scrollbox');
+	contentArea.append(pageHeading, parserOutput);
+	document.body.append(contentArea);
 
 	function beep0(){
+		const beepZero = new Audio('/lcars-database/resources/beep-0.mp3');
 		beepZero.play();
 	}
 
 	function beep1(){
+		const beepOne = new Audio('/lcars-database/resources/beep-1.mp3');
 		beepOne.play();
 	}
 
 	function beep2(){
+		const beepTwo = new Audio('/lcars-database/resources/beep-2.mp3');
 		beepTwo.play();
 	}
 
 	function stardateCalculator(selector){
-		document.querySelectorAll(selector).forEach(function(stardate){
-			const date = (selector === '.stardate') ? new Date(stardate.getAttribute('data-date')).getTime() : new Date().getTime();
+		document.querySelectorAll(selector).forEach(stardate => {
+			const date = selector === '.stardate' ? new Date(stardate.getAttribute('data-date')).getTime() : new Date().getTime();
 			stardate.innerHTML = Math.round(Math.log(date + 432043200000000000000) / Math.log(1.0000001));
 		});
 	}
